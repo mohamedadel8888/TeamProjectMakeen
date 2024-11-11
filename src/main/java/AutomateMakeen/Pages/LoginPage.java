5736db63e985fad444bf37f4c26907b73419a8b3
marwankhalil99 <marwan.shamso2022@gmail.com>
2024-11-11 13:04:11 +0200

    create account test and V17

--- src/main/java/AutomateMakeen/Pages/LoginPage.java
+++ src/main/java/AutomateMakeen/Pages/LoginPage.java
@@ -46,7 +46,7 @@ public class LoginPage extends BaseComp {
     }
 
     public void goToLoginPage(){
-        driver.get("https://em.alqemam.com/qCMS_Test_v13.4.15 ");
+        driver.get("https://em.alqemam.com/qCMS_Test_v13.4.17 ");
     }
     public HomePage loginUserWithoutRemMe(String id , String password)
     {
