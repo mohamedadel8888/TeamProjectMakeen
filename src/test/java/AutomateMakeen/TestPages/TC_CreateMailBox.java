899198cc9d478511fb7eb4238025dfac0b2da939
marwankhalil99 <marwan.shamso2022@gmail.com>
2024-11-12 14:13:56 +0200

    Maro

--- src/test/java/AutomateMakeen/TestPages/TC_CreateMailBox.java
+++ src/test/java/AutomateMakeen/TestPages/TC_CreateMailBox.java
@@ -10,6 +10,9 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TC_CreateMailBox extends TestInit {
+    private String subject;
+    private String docType;
+    private String
     CreateExternalMailPage createExternalMailPage;
 
     @BeforeClass(description = "Preconditions for each test in the class :" +
@@ -27,7 +30,7 @@ public class TC_CreateMailBox extends TestInit {
 
     @Test(dataProvider = "docTypeDataProvider")
     public void tc_createValidExternalMail(String docTypeDP) {
-
+        createExternalMailPage.enteringTheSubjectOfMail();
     }
 
     @DataProvider(name = "docTypeDataProvider")
