3a25df9813fadbd6de6721e4ebb3b6d3630aa9f4
Mohamed.Galal <mohamedadelgalal00@gmail.com>
2025-03-12 15:31:03 +0200

    v 4 .19

--- src/main/java/AutomateMakeen/Pages/EmployeesOperations.java
+++ src/main/java/AutomateMakeen/Pages/EmployeesOperations.java
@@ -2,10 +2,12 @@ package AutomateMakeen.Pages;
 
 import AutomateMakeen.Base.BaseComp;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -35,11 +37,22 @@ public class EmployeesOperations extends BaseComp {
         return driver.findElement(divNewEmployee);
     }
 
+
+    /****************************************************************************************/
+                                       /*رقم الهوية*/
+    /****************************************************************************************/
+
     private By nationNumberText = By.id("txt_AddNation");    /*رقم الهوية*/
     public void addNationNumber(String id) {
         WebElement nationNumber = driver.findElement(nationNumberText);
+        nationNumber.clear();
         nationNumber.sendKeys(id);
     }
+    public WebElement getNationalNumber() {
+        return driver.findElement(nationNumberText);
+    }
+
+
     private By masarButton = By.id("masarButtonT3een"); /*زر استدعاء البيانات من منصه مسار*/
     public WebElement masarBtn(){
         WebElement masarButton1 = driver.findElement(masarButton);
@@ -55,6 +68,101 @@ public class EmployeesOperations extends BaseComp {
         WebElement validationMessage = driver.findElement(notInMasarValidation);
         return validationMessage.getText();
     }
+
+    /****************************************************************************************/
+                                         /*الاسم*/
+    /****************************************************************************************/
+    private By firstName = By.id("txt_AddEmpFrstName");
+    private By secondName = By.id("txt_AddEmpSecName");
+    private By thirdName = By.id("txt_AddEmpThrdName");
+    private By lastName = By.id("txt_AddEmpFrthName");
+
+
+    public void addFirstName (String first){
+        WebElement firstNameInput = driver.findElement(firstName);
+        firstNameInput.clear();
+        firstNameInput.sendKeys(first);
+    }
+    public String getFirstName (){
+        return driver.findElement(firstName).getAttribute("value");
+    }
+
+    public void addSecondName(String second){
+        WebElement secondNameInput = driver.findElement(secondName);
+        secondNameInput.clear();
+        secondNameInput.sendKeys(second);
+    }
+    public String getSecondName(){
+        return driver.findElement(secondName).getAttribute("value");
+    }
+    public void addThirdName(String third){
+        WebElement thirdNameInput = driver.findElement(thirdName);
+        thirdNameInput.clear();
+        thirdNameInput.sendKeys(third);
+    }
+    public String getThirdName(){
+        return driver.findElement(thirdName).getAttribute("value");
+    }
+
+    public void addFourthName(String fourth){
+        WebElement fourthNameInput = driver.findElement(lastName);
+        fourthNameInput.clear();
+        fourthNameInput.sendKeys(fourth);
+    }
+    public String getFourthName(){
+        return driver.findElement(lastName).getAttribute("value");
+    }
+
+    /****************************************************************************************/
+                                        /*تاريخ الميلاد*/
+    /****************************************************************************************/
+
+    private By calendarIcon = By.cssSelector(".fa.fa-calendar[onclick=\"CalenderObj.showCalender('txt_AddEmpBoDate',true, prs_greg_date!=1?'Hij':'Georg')\"]");
+    private By dateText = By.id("txt_AddEmpBoDate");
+
+    public void dateSelectionFromIcon(String day, String month , String year){
+        WebElement calenderIcon = driver.findElement(calendarIcon);
+        calenderIcon.click();
+        WebElement month1 = driver.findElement(By.id("drp_Month"));
+        WebElement year1 = driver.findElement(By.id("tb_Year"));
+        Select monthAll = new Select(month1);
+        monthAll.selectByValue(month);
+        Actions actions =new Actions(driver);
+        actions.click(year1)
+                .keyDown(Keys.CONTROL)
+                .sendKeys("a").keyUp(Keys.CONTROL)
+                .sendKeys(Keys.DELETE)
+                .sendKeys(year)
+                .perform();
+        WebElement day1 = driver.findElement(By.xpath("//input[@value="+day+"][@class='btn']"));
+        day1.click();
+    }
+    public void dateSelect (String day, String month, String year){
+        WebElement dateText1 = driver.findElement(dateText);
+        dateText1.clear();
+        dateText1.sendKeys(year+"/"+month+"/"+day);
+        Actions actions = new Actions(driver);
+        actions.moveByOffset(50, 50).click().perform();
+    }
+    public String getDateText(){
+        WebElement dateText1 = driver.findElement(dateText);
+        return dateText1.getAttribute("value");
+    }
+
+    /****************************************************************************************/
+                                        /* IBAN */
+    /****************************************************************************************/
+    private By ibanText = By.id("txt_AddBankNumber");
+    public void addIBAN(String iban){
+        WebElement ibanInput = driver.findElement(ibanText);
+        ibanInput.clear();
+        ibanInput.sendKeys(iban);
+    }
+    public String getIBAN(){
+        return driver.findElement(ibanText).getAttribute("value");
+    }
+
+
 
 
 
--- src/test/java/AutomateMakeen/TestPages/TC_EmployeeOperations.java
+++ src/test/java/AutomateMakeen/TestPages/TC_EmployeeOperations.java
@@ -9,6 +9,8 @@ import org.testng.annotations.Test;
 
 import java.io.FileNotFoundException;
 import java.time.Duration;
+import java.time.chrono.HijrahDate;
+import java.time.format.DateTimeFormatter;
 
 public class TC_EmployeeOperations extends TestInit {
     WebDriverWait exWait;
@@ -17,8 +19,26 @@ public class TC_EmployeeOperations extends TestInit {
     String notInMasarValidation;
     String notValidNationNumber;
     String notValidNumberValidation;
+    String biggerThan10Numbers;
 
+    String firstName;
+    String secondName;
+    String thirdName;
+    String fourthName;
 
+    String firstNameNotValid;
+    String secondNameNotValid;
+    String thirdNameNotValid;
+    String fourthNameNotValid;
+
+    String day;
+    String month;
+    String year;
+    String dayNotValid;
+    String monthNotValid;
+
+    String IBAN;
+    String IBANNotValid;
 
     /** preconditions :
      السماحية للدخول الي النظام .
@@ -39,18 +59,35 @@ public class TC_EmployeeOperations extends TestInit {
         notInMasarValidation=(getJsonData("EmployeeOperations","notInMasarValidation"));
         notValidNationNumber=(getJsonData("EmployeeOperations","notValidNationNumber"));
         notValidNumberValidation = (getJsonData("EmployeeOperations","notValidNumberValidation"));
+        biggerThan10Numbers = (getJsonData("EmployeeOperations","biggerThan10Numbers"));
+        firstName = (getJsonData("EmployeeOperations","firstName"));
+        secondName = (getJsonData("EmployeeOperations","secondName"));
+        thirdName = (getJsonData("EmployeeOperations","thirdName"));
+        fourthName = (getJsonData("EmployeeOperations","fourthName"));
+        firstNameNotValid = (getJsonData("EmployeeOperations","firstNameNotValid"));
+        secondNameNotValid = (getJsonData("EmployeeOperations","secondNameNotValid"));
+        thirdNameNotValid = (getJsonData("EmployeeOperations","thirdNameNotValid"));
+        fourthNameNotValid = (getJsonData("EmployeeOperations","fourthNameNotValid"));
+        day = (getJsonData("EmployeeOperations","day"));
+        month = (getJsonData("EmployeeOperations","month"));
+        dayNotValid = (getJsonData("EmployeeOperations","dayNotValid"));
+        monthNotValid = (getJsonData("EmployeeOperations","monthNotValid"));
+        year = (getJsonData("EmployeeOperations","year"));
+        IBAN = (getJsonData("EmployeeOperations","IBAN"));
+        IBANNotValid = (getJsonData("EmployeeOperations","IBANNotValid"));
     }
+
     @Test (priority = 1)
-    public void verifyEmployeeOperations (){
+    public void verifyEmployeeOperations (){   /*التحقق من فتح صفحه عمليات على الموظفين*/
         Assert.assertTrue(employeesOperations.getEmpOperations().isDisplayed());
     }
     @Test (priority = 2)
-    public void verifyAddEmployeeTab (){
+    public void verifyAddEmployeeTab (){   /*التحقق من الدخول الى شاشة تعيين موظف*/
         employeesOperations.enterAppointEmployee();
         Assert.assertTrue(employeesOperations.getDivNewEmployee().isDisplayed());
     }
     @Test (priority = 3)
-    public void verifyNationNumberNotInMasar (){
+    public void verifyNationNumberNotInMasar (){  /*التحقق من فالديشن رقم الهوية غير موجود في مسار*/
         //employeesOperations.enterAppointEmployee();
         employeesOperations.addNationNumber(nationNumberNotInMasar);
         exWait.until(ExpectedConditions.elementToBeClickable(employeesOperations.masarBtn()));
@@ -58,12 +95,67 @@ public class TC_EmployeeOperations extends TestInit {
         Assert.assertEquals(notInMasarValidation,employeesOperations.getValidationMessage());
     }
     @Test (priority = 4)
-    public void verifyNotValidNationNumber (){
+    public void verifyNotValidNationNumber (){ /*التحقق من فالديشن رقم الهوية يبدأ بصفر*/
        // employeesOperations.enterAppointEmployee();
         employeesOperations.addNationNumber(notValidNationNumber);
         exWait.until(ExpectedConditions.elementToBeClickable(employeesOperations.masarBtn()));
         employeesOperations.masarBtn().click();
         Assert.assertEquals(notValidNumberValidation,employeesOperations.getValidationMessage());
     }
-
+    @Test (priority = 5)
+    public void verifyNationNumberBiggerThan10Num (){ /*التحقق من ادخال رقم الهوية  اكثر من 10 ارقام*/
+        //employeesOperations.enterAppointEmployee();
+        employeesOperations.addNationNumber(biggerThan10Numbers);
+        Assert.assertNotEquals(biggerThan10Numbers,employeesOperations.getNationalNumber().getText());
+    }
+    @Test (priority = 6)
+    public void verifyFullNameNotValid (){ /*التحقق من ادخال الاسم غير صحيح*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.addFirstName(firstNameNotValid);
+        Assert.assertEquals(employeesOperations.getFirstName(),"");
+        employeesOperations.addSecondName(secondNameNotValid);
+        Assert.assertEquals(employeesOperations.getSecondName(),"");
+        employeesOperations.addThirdName(thirdNameNotValid);
+        Assert.assertEquals(employeesOperations.getThirdName(),"");
+        employeesOperations.addFourthName(fourthNameNotValid);
+        Assert.assertEquals(employeesOperations.getFourthName(),"");
+    }
+    @Test (priority = 7)
+    public void verifyFullNameValid (){ /*التحقق من ادخال الاسم صحيح*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.addFirstName(firstName);
+        Assert.assertEquals(employeesOperations.getFirstName(),firstName);
+        employeesOperations.addSecondName(secondName);
+        Assert.assertEquals(employeesOperations.getSecondName(),secondName);
+        employeesOperations.addThirdName(thirdName);
+        Assert.assertEquals(employeesOperations.getThirdName(),thirdName);
+        employeesOperations.addFourthName(fourthName);
+        Assert.assertEquals(employeesOperations.getFourthName(),fourthName);
+    }
+    @Test (priority = 8)
+    public void verifyDateOfBirthNotValid (){ /*التحقق من ادخال التاريخ غير صحيح*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.dateSelect(dayNotValid,monthNotValid,year);
+        HijrahDate today = HijrahDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");  // Adjust format as needed
+        String expectedDate = today.format(formatter);
+        Assert.assertEquals(employeesOperations.getDateText(),expectedDate);
+    }
+    @Test (priority = 9)
+    public void verifyDateOfBirthValid (){ /*التحقق من ادخال التاريخ صحيح*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.dateSelectionFromIcon(day,month,year);
+        Assert.assertEquals(employeesOperations.getDateText(),year+"/0"+month+"/0"+day);
+    }
+    @Test (priority = 10)
+    public void verifyIBAN_Valid (){ /*التحقق من ادخال رقم البطاقة البنكية صحيح*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.addIBAN(IBAN);
+        Assert.assertEquals(employeesOperations.getIBAN(),IBAN);
+    }
+    @Test (priority = 11)
+    public void verifyIBAN_NotValid(){ /*التحقق من ادخال رقم البطاقة البنكية اكبر من  30*/
+        employeesOperations.enterAppointEmployee();
+        employeesOperations.addIBAN(IBANNotValid);        Assert.assertNotEquals(employeesOperations.getIBAN(),IBANNotValid);
+    }
 }
